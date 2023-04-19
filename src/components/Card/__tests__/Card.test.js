1b3de0d5180110293ac142321316887b6dce604a
Grethel Bello Cagnant <bello.grethel@gmail.com>
2023-04-18 21:18:05 -0500

    feat: Add component tests

--- src/components/Card/__tests__/Card.test.js
+++ src/components/Card/__tests__/Card.test.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import Card from '../index';
+import pokemon from './pokemon.json';
+
+describe('components/Card', () => {
+    it('renders correctly', () => {
+        render(<Card pokemon={pokemon} />);
+    });
+});
